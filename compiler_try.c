1d229b6db1659ef209db98d4cbc14b2f9d2d0e9e
unknown <yuehan@kth.se>
2020-11-21 14:51:28 +0100

    virtual machine building (2)

---
+++ compiler_try.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+
+int token;            // current token
+char *src, *old_src;  // pointer to source code string;
+int poolsize;         // default size of text/data/stack
+int line;             // line number
+                                                // (2)
+int *text,            // code                  
+    *old_text,        // for dump text segment
+    *stack;           // 局部变量，函数的参数值
+char *data;           // 全局变量
+
+int *pc, *bp, *sp, ax, cycle; // virtual machine registers
+
+
+
+void next() {
+    token = *src++;
+    return;
+}
+
+void expression(int level) {
+    // do nothing
+}
+
+void program() {
+    next();                  // get next token
+    while (token > 0) {
+        printf("token is: %c\n", token);
+        next();
+    }
+}
+
+
+int eval() { // do nothing yet
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    int i, fd;
+
+    argc--;
+    argv++;
+
+    poolsize = 256 * 1024; // arbitrary size
+    line = 1;
+
+    if ((fd = open(*argv, 0)) < 0) {
+        printf("could not open(%s)\n", *argv);
+        return -1;
+    }
+
+    if (!(src = old_src = malloc(poolsize))) {
+        printf("could not malloc(%d) for source area\n", poolsize);
+        return -1;
+    }
+
+    // read the source file
+    if ((i = read(fd, src, poolsize-1)) <= 0) {
+        printf("read() returned %d\n", i);
+        return -1;
+    }
+    src[i] = 0; // add EOF character
+    close(fd);
+
+                                                                    // (2) allocate memory for virtual machine
+    if (!(text = old_text = malloc(poolsize))) {
+        printf("could not malloc(%d) for text area\n", poolsize);
+        return -1;
+    }
+    if (!(data = malloc(poolsize))) {
+        printf("could not malloc(%d) for data area\n", poolsize);
+        return -1;
+    }
+    if (!(stack = malloc(poolsize))) {
+        printf("could not malloc(%d) for stack area\n", poolsize);
+        return -1;
+    }
+
+    memset(text, 0, poolsize);//initiate the memory
+    memset(data, 0, poolsize);
+    memset(stack, 0, poolsize);
+
+    bp = sp = (int *)((int)stack + poolsize);//SP，指针寄存器，永远指向栈顶，“stack”是水位溢出警戒线
+    ax = 0;////////////
+
+    program();
+    return eval();
+}
